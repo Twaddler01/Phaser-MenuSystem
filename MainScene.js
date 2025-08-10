07b6c70e60927edb1463211bf2051fed0883fea2
Twaddler01 <kenleydad@gmail.com>
2025-08-10 03:06:04 -0500

    main

--- MainScene.js
+++ MainScene.js
@@ -1,11 +1,11 @@
-//import MenuItem from './MenuItem.js';
+import MenuSystem from './MenuSystem.js';
 
 // `000`
 // console.log();
 
 class MainScene extends Phaser.Scene {
     constructor() {
-        super({ key: 'MainScene' });
+        super('MainScene');
     }
 
     preload() {
@@ -27,7 +27,42 @@ class MainScene extends Phaser.Scene {
         this.graphics.setDepth(-1); // -1 ensures it's behind other game elements
 
         //
-        
+
+const menuData = {
+    parent: [
+        { id: 'menu1', content: [
+            { id: 'menu1content1', title: 'Menu 1 - Contents 1', bgColor: 0x333333, action: 'act1.1' },
+            { id: 'menu1content2', title: 'Menu 1 - Contents 2', bgColor: 0x333333, action: 'act1.2' },
+            { id: 'menu1content3', title: 'Menu 1 - Contents 3', bgColor: 0x333333, action: 'act1.3' },
+        ] },
+        { id: 'menu2', content: [
+            { id: 'menu2content1', title: 'Menu 2 - Contents 1', bgColor: 0x333333, action: 'act2.1' },
+            { id: 'menu2content2', title: 'Menu 2 - Contents 2', bgColor: 0x333333, action: 'act2.2' },
+            { id: 'menu2content3', title: 'Menu 2 - Contents 3', bgColor: 0x333333, action: 'act2.3' },
+        ] },
+        { id: 'menu3', content: [
+            { id: 'menu3content1', title: 'Menu 3 - Contents 1', bgColor: 0x333333, action: 'act3.1' },
+            { id: 'menu3content2', title: 'Menu 3 - Contents 2', bgColor: 0x333333, action: 'act3.2' },
+            { id: 'menu3content3', title: 'Menu 3 - Contents 3', bgColor: 0x333333, action: 'act3.3' },
+        ] }
+    ]
+};
+
+const myMenuSystem = new MenuSystem(this, {
+    data: menuData,
+    x: 50,
+    y: 50,
+    width: 300,
+    itemHeight: 40,
+    contentIndent: 0,
+    verticalPadding: 8
+});
+
+
+
+
+
+
 
     } // create()
 } // MainScene
