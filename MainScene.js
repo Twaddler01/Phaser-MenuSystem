b7df5a12e30dda3240070b9305d9dd4ba8eed74d
Twaddler01 <kenleydad@gmail.com>
2025-08-20 21:48:10 -0500

    main

--- MainScene.js
+++ MainScene.js
@@ -33,34 +33,51 @@ class MainScene extends Phaser.Scene {
         this.graphics.fillRect(0, 0, width, height);
         this.graphics.setDepth(-1); // -1 ensures it's behind other game elements
 
-        //// MENUS + CONTENT
-        const menu = new MenuSystem({ x: 50, y: 50, width: 350, itemHeight: 50, contentIndent: 0 });
+//// MENUS + CONTENT
 
+// Define menu here...
+        const menu = new MenuSystem({ 
+            x: 50, // Menu location x
+            y: 50, // Menu location y
+            width: 350, // Menu width
+            itemHeight: 50, // Default height of each menu item
+            contentIndent: 0 }); // Indentation of all content relative to parent
+
+// Add content here and parent menu if not initialized....
+// TYPES:
+// RectObj ... a rectangle with color
+// TextObj ... text with color
+// ImgObj .... an image as specified in preload()
+
+
+        // Add image
         menu.addItem(ImgObj, { 
-            parentMenu: 'Menu 4', 
-            imageKey: 'opened', 
+            parentMenu: 'Menu 1', // Title and ID
+            imageKey: 'opened',  // Define img source in preload() function above
             imgWidth: 50, 
             imgHeight: 50, 
             bgColor: 0x444444, 
             onClick: (obj) => console.log(`ImgObj clicked: ID ${obj.id}`)
         });
-
+        
+        // Add over other existing menu content
         menu.addItemOver(TextObj, { 
-            parentMenu: 'Menu 4', 
-            text: 'Content Text for Menu 4...',
+            parentMenu: 'Menu 1', 
+            text: 'Content Text for Menu 1...',
             bgColor: 0x444444, 
             onClick: (obj) => console.log(`TextObj clicked: ID ${obj.id}`)
         });
 
+        // Rectangle is automatically created if only an overlay item is added
         menu.addItemOver(TextObj, { 
-            parentMenu: 'Menu 3', 
-            text: 'Content Text for Menu 3...',
+            parentMenu: 'Menu 2', 
+            text: 'Content Text for Menu 2...',
             bgColor: 0x444444, 
             onClick: (obj) => console.log(`TextObj clicked: ID ${obj.id}`)
         });
         
         menu.addItem(ImgObj, { 
-            parentMenu: 'Menu 5', 
+            parentMenu: 'Menu 3', 
             imageKey: 'opened', 
             stretch: { imgWidth: 100 }, 
             bgColor: 0x444444,
@@ -68,14 +85,14 @@ class MainScene extends Phaser.Scene {
         });
         
         menu.addItem(ImgObj, { 
-            parentMenu: 'Menu 6', 
+            parentMenu: 'Menu 4', 
             imageKey: 'opened', 
             bgColor: 0x666666,
             onClick: (obj) => console.log(`ImgObj clicked: ID ${obj.id}`)
         });
         
         menu.addItem(ImgObj, { 
-            parentMenu: 'Menu 7', 
+            parentMenu: 'Menu 5', 
             imageKey: 'opened', 
             fixed: { imgWidth: 100 }, 
             bgColor: 0x444444,
@@ -83,10 +100,11 @@ class MainScene extends Phaser.Scene {
         });
         
         menu.addItem(RectObj, { 
-            parentMenu: 'Menu 8', 
+            parentMenu: 'Menu 6', 
             onClick: (obj) => console.log(`RectObj clicked: ID ${obj.id}`)
         });
 
+        // Run after all menus initialized
         menu.render();
 
 
